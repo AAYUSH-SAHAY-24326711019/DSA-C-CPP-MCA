587caea55f3ea60c52cb0dfb97d0ee07babbc2ae
AAYUSH-SAHAY-24326711019 <aayush.mcastudent24.1062@cimage.in>
2025-09-16 20:12:44 +0530

    Adding the ins_end() & ins_beg() to insert nodes, to traverse trav()

--- singleLinkedList/single.c
+++ singleLinkedList/single.c
@@ -16,9 +16,64 @@ void c_list(N **head){
 }
 //----------[Task 2]-----------
 
+
+//---------[Task 3]---------
+//1. inserting at the end
+    void ins_end(N** head, int item){
+        //1.1 create a node and location pointer
+            N *ptr, *loc;
+            ptr=(N*)malloc(sizeof(N));
+            ptr->info=item; //providing the value to the info field
+            ptr->next=NULL; // making the next field null
+
+        //1.2 if list empty make it the first node
+        if(*head ==NULL){
+            *head=ptr;
+        }
+        //1.3 else traverse to the final node and attach it,
+        else{
+            loc=*head; //initially start from head pointer
+            while(loc->next!=NULL){
+                loc=loc->next;
+            }//exit from loop when the last node next field is found null
+            loc->next=ptr; // assign address of the ptr to the next field of last node
+        }
+
+    }
+
+    void trav(N *head){
+        while(head!=NULL){
+            printf("[%2d]",head->info);
+            printf("-");
+            head=head->next;
+        }
+    }
+
+    //2. Adding a function to insert at beginning
+
+    void ins_beg(N** head, int item){
+        //2.1 make a node
+        N *ptr = (N*)malloc(sizeof(N));
+        //2.2 provide the input to the field.
+        ptr->info= item;
+        //2.3 if the head is null the make it the first node
+        if(*head==NULL){
+            ptr->next=NULL; //then make the next field as null
+        }
+        //2.4 provide the address of the node of node ->next
+        else{
+            ptr->next=*head;
+        }
+        //2.5 finally make the node the 1st node by
+        *head=ptr;
+    }
+
+//---------[Task 3]---------
+
 int main(){
     int key = 1;
-    int choice =0;
+    int choice =0,input=0;
+    int choice1=0;
     while(key){
         printf("\n1. Press 1 to create a single linklist ");
         printf("\n2. Press 2 to insert a node in single linklist (starting)");
@@ -38,11 +93,42 @@ int main(){
             break;
 
             case 2 :
+                
                 printf("\ninserting a node at start \n");
+                printf("\n\t1. Press 1 to insert from the end.");
+                printf("\n\t2. Press 2 to insert from the start.");
+                printf("\n\t3. Press 0 to go back");
+                printf("\n\tUser give input = ");
+                scanf("%d",&choice1);
+                switch(choice1){
+                    case 0:
+                    break;
+
+                    case 1:
+                        printf("\nAdding element at end\n");
+                        printf("\nUser provide input =");
+                        scanf("%d",&input);
+                        ins_end(&head,input);
+                    break;
+
+                    case 2:
+                        printf("\nAdding element at start\n");
+                        printf("\nUser provide input =");
+                        scanf("%d",&input);
+                        ins_beg(&head,input);
+                    break;
+
+                    default:
+                        break;
+
+                }
+
+
             break;
 
             case 3 :
                 printf("\ntraversing a single linklist\n");
+                trav(head);
             break;
             
             default:
