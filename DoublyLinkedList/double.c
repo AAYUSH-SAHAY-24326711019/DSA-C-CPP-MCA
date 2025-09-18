cfb6d33b8c9f19e0f76556a6444e301d5cc6ede9
AAYUSH-SAHAY-24326711019 <aayush.mcastudent24.1062@cimage.in>
2025-09-18 07:48:43 +0530

    Added the menu to the doubly linklist program

---
+++ DoublyLinkedList/double.c
@@ -0,0 +1,43 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(){
+    int key=1,choice1=0;
+    while(key){
+        printf("\nUser follow the instructions.");
+        printf("\n1. Press 1 to create a Doubly LinkedList");
+        printf("\n2. Press 2 to insert nodes.");
+        printf("\n3. Press 3 to traverse the list.");
+        printf("\n4. Press 4 to implement deletion process");
+        printf("\n PRESS 0 TO EXIT.");
+        printf("\nUser give input =");
+        scanf("%d",&choice1);
+        switch(choice1){
+            case 0:
+                printf("\n\tExiting the program ...");
+                key=0;
+            break;
+
+            case 1:
+                printf("\n\t Created a Doubly Linked List\n");
+            break;
+
+            case 2:
+                printf("\n\t inserting nodes in Doubly Linked List\n");
+            break;
+
+            case 3:
+                printf("\n\t Traversing the Doubly Linked List\n");
+            break;
+
+            case 4:
+                printf("\n\t Implementing Deletion in Doubly Linked List\n");
+            break;
+
+        }
+
+    }
+
+
+    return 0;
+}
